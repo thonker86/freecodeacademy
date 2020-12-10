7e00d5aa77015d9086199c8343d56f95883d165d
thonker86 <thekingrock@gmail.com>
2020-12-10 12:26:40 +0200

    Create find-the-symmetric-difference.js
    <a href="#">test</a>
    https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/find-the-symmetric-difference

---
+++ find-the-symmetric-difference.js
@@ -0,0 +1,26 @@
+function sym(...args) {
+  let proArr = []
+  for(let arg of args){
+    proArr.push(arg.reduce((acc, x) => {
+    if(!acc.includes(x)){
+      acc.push(x)
+    }
+    return acc
+  },[]))
+  }
+  console.log(proArr)
+  let result = proArr.reduce((acc, arr) => {
+    for(let ele of arr){
+      if(acc.includes(ele)){
+        acc.splice(acc.indexOf(ele), 1)
+      }else{    
+        acc.push(ele)
+      }
+    }
+    return acc
+  })
+  console.log(result.sort())
+  return result.sort()
+}
+
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]);
