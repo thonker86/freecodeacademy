9bf620814e2d17b7864f1f46b327841e394a2cf6
thonker86 <thekingrock@gmail.com>
2020-12-14 14:23:56 +0200

    Create perform-an-intersection-on-two-sets-of-data.js
    https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/perform-an-intersection-on-two-sets-of-data

---
+++ Data Structures/perform-an-intersection-on-two-sets-of-data.js
@@ -0,0 +1,68 @@
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // This method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // This method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // This method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // This is our union method 
+  union(set) {
+    const newSet = new Set();
+    this.values().forEach(value => {
+      newSet.add(value);
+    })
+    set.values().forEach(value => {
+      newSet.add(value);
+    })
+
+    return newSet;
+  }
+  // Only change code below this line
+  intersection(set){
+    let interSet = new Set();
+    let largeSet = set;
+    let smallSet = this;
+    if(this.size() > set.size()){
+      largeSet = this;
+      smallSet = set;
+    }
+    smallSet.values().forEach(el => {
+      if(largeSet.has(el)){
+        interSet.add(el)
+      }
+    })
+    return interSet;
+  }
+  // Only change code above this line
+}
