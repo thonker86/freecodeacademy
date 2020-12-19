d536872af9fd2854664e1ec584c9d11632ee18b1
thonker86 <thekingrock@gmail.com>
2020-12-19 15:18:04 +0200

    Create check-if-an-element-is-present-in-a-binary-search-tree.js
    https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/check-if-an-element-is-present-in-a-binary-search-tree

---
+++ Data Structures/check-if-an-element-is-present-in-a-binary-search-tree.js
@@ -0,0 +1,68 @@
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+function BinarySearchTree() {
+  this.root = null;
+  // Only change code below this line
+  this.add = (value, root = this.root)=>{
+    let newNode = new Node(value);
+    if(!this.root){
+      this.root = newNode;
+      return;
+    }else{
+      if(value == root.value){
+        return null
+      }else if(value < root.value){
+        if(!root.left){
+          root.left = newNode;
+        }else{
+          return this.add(value, root.left)
+        }
+      }else{
+        if(!root.right){
+          root.right = newNode;
+        }else{
+          return this.add(value, root.right)
+        }
+      }
+    }
+  }
+  
+  this.isPresent = value=>{
+    if(!this.root){
+      return false;
+    }
+    let current = this.root;
+    while(current){
+      if(current.value == value){
+        return true;
+      }else{
+        if(value > current.value){
+          current = current.right
+        }else{
+          current = current.left
+        }
+      }
+    }
+    return false;
+  }
+  // Only change code above this line
+}
+
+let tree = new BinarySearchTree();
+//console.log(tree);
+tree.add(3)
+tree.add(4)
+tree.add(6)
+tree.add(7)
+tree.add(5)
+tree.add(2)
+tree.add(1)
+tree.add(10)
+console.log(tree.isPresent(9))
+//console.log(tree.add(6))
+console.log(tree.root);
+//console.log(displayTree(tree))
