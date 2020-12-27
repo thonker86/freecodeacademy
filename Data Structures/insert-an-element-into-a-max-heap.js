e19b1277403867e48d0316dd78538f5c7ece2bd8
thonker86 <thekingrock@gmail.com>
2020-12-27 20:49:44 +0200

    Update insert-an-element-into-a-max-heap.js
    Correct one

--- Data Structures/insert-an-element-into-a-max-heap.js
+++ Data Structures/insert-an-element-into-a-max-heap.js
@@ -1,34 +1,16 @@
 var MaxHeap = function() {
   this.heap = [null];
-  this.getParentIndex = (index)=>{
-    return Math.floor(index/2) == 0 ? 1 : Math.floor(index/2);
-  };
-  this.getLeftIndex = (index)=>{
-    return index * 2;
-  };
-  this.getRightIndex = (index)=>{
-    return index * 2 + 1;
-  }
-  this.insert = (data, index = 1)=>{
-    if(this.heap.length == 1){
-      return this.heap.push(data)
-    }else{
-      if(data > this.heap[index]){
-        return this.heap.splice(index, 0, data)
-      }else{
-        let leftIndex = this.getLeftIndex(index);
-        let rightIndex = this.getRightIndex(index);
-        if(this.heap[leftIndex] === undefined || this.heap[rightIndex] === undefined){
-          return this.heap.push(data)
-        }else{
-          if(!this.insert(data, leftIndex)){
-            this.insert(data, rightIndex);
-          }
+  this.insert = (data)=>{
+      this.heap.push(data);
+      let heap = this.heap;
+      (function maxHeap(index){
+        if(data > heap[Math.floor(index/2)] & index > 1){
+          [heap[index], heap[Math.floor(index/2)]] = [heap[Math.floor(index/2)], data]
+          maxHeap(Math.floor(index/2))
         }
-      }
-    }
+      })(this.heap.length-1)
   };
   this.print = ()=>{
-    return this.heap
-  };
+    return this.heap;
+  }
 };
